e489d8635019a83619143a57bc3037eba74f1e85
Ariel Zelivansky <ariel@twistlock.com>
2018-08-22 20:35:57 +0300

    Implementation progress

--- ext/kisaten/kisaten.c
+++ ext/kisaten/kisaten.c
@@ -13,6 +13,11 @@
 
 #endif
 
+/* General TODO:
+   * Replace all rb_eRuntimeError with a Kisaten error type
+   * Consider treating AFL_INST_RATIO one day
+*/
+
 /* Constants that must be in sync with afl-fuzz (afl/config.h) */
 #define AFL_SHM_ENV_VAR     "__AFL_SHM_ID"
 #define AFL_FORKSRV_FD      198
@@ -30,31 +35,33 @@ uint8_t use_forkserver = 0;
 uint8_t kisaten_init_done = 0;
 uint8_t afl_persistent_mode = 0;
 
-static void kisaten_map_shm() 
+static inline void kisaten_map_shm() 
 {
     char *shm_id_str = NULL;
     int shm_id = 0;
 
     if (NULL != afl_area_ptr)
     {
-        rb_raise("Kisaten error: kisaten_map_shm was called but afl_area_ptr is not NULL");
+        rb_raise(rb_eRuntimeError, "Kisaten error: kisaten_map_shm was called but afl_area_ptr is not NULL");
     }
 
-    shm_id_str = getenv(SHM_ENV_VAR);
+    shm_id_str = getenv(AFL_SHM_ENV_VAR);
 
     if (!shm_id_str)
     {
         /* rb_warning only prints if $VERBOSE is true. 
-        This could allow debugging here while not interfering with the program too much */
+           This could allow info here while not interfering with the program too much.
+           TODO: Allow passing when needed with a flag in init/env var
+         */
         rb_warning("Kisaten failed to get AFL shared memory environment variable");
     }
     else
     {
         errno = 0;
-        shm_id = strtol(id_str, NULL, 10); /* Safer atoi */
+        shm_id = strtol(shm_id_str, NULL, 10); /* Safer atoi */
         if (0 != errno)
         {
-            rb_raise(rb_eRuntimeError, "Kisaten failed to read valid integer from SHM_ENV_VAR!");
+            rb_raise(rb_eRuntimeError, "Kisaten failed to read valid integer from AFL_SHM_ENV_VAR!");
         }
 
         afl_area_ptr = shmat(shm_id, NULL, 0);
@@ -111,20 +118,17 @@ static void kisaten_scope_event(VALUE self, void *data)
  
     trace_arg = rb_tracearg_from_tracepoint(self);
 
-    /* Are rb_tracearg_method_id or rb_tracearg_binding helpful for instrumentation? */
+    /* Maybe: are rb_tracearg_method_id or rb_tracearg_binding helpful for instrumentation? */
     path = rb_tracearg_path(trace_arg); /* String path */
     lineno = rb_tracearg_lineno(trace_arg); /* Integer line */
 
-    if (NULL == afl_area)
+    if (NULL != afl_area_ptr)
     {
-        rb_raise(rb_eRuntimeError, "Kisaten dangerous bug: afl_area not set");
+        /* Refer to afl's technical_details.txt, 1 (Coverage measurements) for info on the injection */
+        _cur_location = kisaten_location_fnv_hash(path, lineno) % AFL_MAP_SIZE;
+        afl_area_ptr[_cur_location ^ prev_location]++;
+        prev_location = _cur_location >> 1;
     }
-
-    /* Refer to afl's technical_details.txt, 1 (Coverage measurements) for info on the injection */
-    _cur_location = kisaten_location_fnv_hash(path, lineno) % AFL_MAP_SIZE;
-    /* TODO: REPLACE THIS PART WITH POINTER */
-    afl_area[_cur_location ^ prev_location]++;
-    prev_location = _cur_location >> 1;
 
     /* TODO: Does it not get triggered by this module's C calls? */
     /* rb_tracepoint_disable(self); */
@@ -146,12 +150,12 @@ static VALUE rb_init_kisaten(VALUE self)
     /* Reset sigaction structs regardless of their use */
     sigemptyset(&_old_sigchld.sa_mask);
     _old_sigchld.sa_flags = 0;
-    _old_sigchld.sigaction = NULL;
+    _old_sigchld.sa_sigaction = NULL;
     _old_sigchld.sa_handler = SIG_DFL;
     
     sigemptyset(&_dfl_sigchld.sa_mask);
     _dfl_sigchld.sa_flags = 0;
-    _dfl_sigchld.sigaction = NULL;
+    _dfl_sigchld.sa_sigaction = NULL;
     _dfl_sigchld.sa_handler = SIG_DFL;
 
     /* Initialization logic begin */
@@ -296,13 +300,12 @@ static VALUE rb_init_kisaten(VALUE self)
     /* TODO: PRIORITY: Exception hook
        accept exception ID in init and set it in MRI */
 
+    /* Get AFL shared memory before starting instrumentation */
     kisaten_map_shm();
 
-
-    /* Find all possible events in ruby.h
-       TODO: TOP PROIROTY: Check if also need other events for scope, such as :thread_begin 
-       TODO: PRIOIRITY: Raise events
-       TODO: PRIORITY: C function calls
+    /* TODO: Find all possible events in ruby.h
+       TODO: PRIOROTY: Check if also need other events for scope, such as :thread_begin 
+       TODO: PRIORITY: Raise events, C function calls
     */
     VALUE tp_scope_event = rb_tracepoint_new(Qnil, RUBY_EVENT_B_CALL |
                                                    RUBY_EVENT_CALL |
